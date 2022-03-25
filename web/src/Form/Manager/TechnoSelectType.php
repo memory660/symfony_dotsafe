15929d7a9270e33669b1674ac6095786d455f72a
duke <webcreation33@yahoo.fr>
2022-03-25 10:10:03 +0100

    AV05

---
+++ web/src/Form/Manager/TechnoSelectType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\Manager;
+
+use App\Entity\Techno;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class TechnoSelectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        
+dump($options['choices']);        
+
+        $builder
+            ->add('techno', ChoiceType::class, [
+                'choices'  => $options['choices']['technos'],
+            ])
+            ->add('member', ChoiceType::class, [
+                'choices'  => $options['choices']['members'],
+            ])            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+
+            'choices' => null,
+        ]);
+    }
+}
